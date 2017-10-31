4ac4c4c29cce86910c66af62dd829b01057495ae
Jose Lopez <jjlz.cocoa@gmail.com>
2017-10-31 13:51:06 -0600

    Ultimos cambios

--- TestIntegracion/testVC.swift
+++ TestIntegracion/testVC.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class testVC: UIViewController {
-
+    
+    @IBOutlet weak var lblForTest: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +23,8 @@ class testVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func btntTapped(_ sender: Any) {
+        
+        lblForTest.textColor = UIColor.red
     }
-    */
-
 }
